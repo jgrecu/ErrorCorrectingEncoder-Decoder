b5f4c0904172c1056f3933306157ae18df5e0209
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-25 10:28:48 +0100

    JBA - Error Correcting Encoder-Decoder - Stage 4/5: Bit-level correction code

--- task/src/correcter/Main.java
+++ task/src/correcter/Main.java
@@ -2,71 +2,179 @@ package correcter;
 
 import java.io.*;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-//        String encodedMessage = encodeMessage(input);
-//        String scrambledMessage = errorEmulator(encodedMessage);
-//        String decodedMessage = decodeMessage(scrambledMessage);
-//        System.out.println(input);
-//        System.out.println(encodedMessage);
-//        System.out.println(scrambledMessage);
-//        System.out.println(decodedMessage);
-        errorEmulator();
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a mode: ");
+        String input = scanner.nextLine();
+        switch (input) {
+            case "encode":
+                encode();
+                break;
+            case "send":
+                send();
+                break;
+            case "decode":
+                decode();
+                break;
+            default:
+                break;
+        }
     }
 
-//    public static String errorEmulator(String string) {
-//        StringBuilder sb = new StringBuilder(string);
-//        Random r = new Random(3);
-//        int size = string.length();
-//        String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//        for (int i = 0; i < size - 2; i += 3) {
-//            int rand1 = r.nextInt(3);
-//            sb.replace(i + rand1, i + rand1 + 1, String.valueOf(chars.charAt(r.nextInt(chars.length()))));
-//        }
-//        return sb.toString();
-//    }
+    public static int getBit(byte b, int index) {
+        return (b & (1 << (7 - index))) == 0 ? 0 : 1;
+    }
 
-    public static void errorEmulator() {
-        Random r = new Random(8);
-        try (
-                InputStream inputStream = new FileInputStream("send.txt");
-                OutputStream outputStream = new FileOutputStream("received.txt");
-        ) {
-            int byteRead;
-            while ((byteRead = inputStream.read()) != -1) {
-                byteRead ^= 1 << r.nextInt(8);
-                outputStream.write(byteRead);
+    public static byte setBit(byte b, int bit, int index) {
+        if (bit == 0) {
+            if (getBit(b, index) == 0) {
+                b = (byte) (b ^ (1 << (7 - index)));
             }
+            return (byte) (b ^ (1 << (7 - index)));
+        }
+        return (byte) (b | (bit << (7 - index)));
+    }
 
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+    public static void encode() {
+        File file = new File("send.txt");
+
+        int numBytes = (int) file.length();
+        int numBits = numBytes * 8;
+        int numOutBytes = numBits / 3 + (numBits % 3 == 0 ? 0 : 1);
+
+        byte[] in_data = new byte[numBytes];
+        byte[] out_data = new byte[numOutBytes];
+
+        try (FileInputStream inputStream = new FileInputStream("send.txt")) {
+            inputStream.read(in_data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (var i : in_data) {
+            System.out.print(i + " ");
+        }
+
+        for (int i = 0; i < numOutBytes; ++i) {
+            int sum = 0;
+            out_data[i] = 0;
+            for (int j = 0; j < 3; ++j) {
+                int numBit = i * 3 + j;
+                int numByte = numBit / 8;
+                int bytePos = numBit % 8;
+                int bit;
+                if (numByte >= in_data.length) {
+                    bit = 0;
+                } else {
+                    bit = getBit(in_data[numByte], bytePos);
+                }
+                sum ^= bit;
+                out_data[i] = setBit(out_data[i], bit, j * 2);
+                out_data[i] = setBit(out_data[i], bit, j * 2 + 1);
+            }
+            out_data[i] = setBit(out_data[i], sum, 6);
+            out_data[i] = setBit(out_data[i], sum, 7);
+        }
+
+        for (var i : out_data) {
+            System.out.print(i + " ");
+        }
+
+        try (OutputStream outputStream = new FileOutputStream("encoded.txt", false)) {
+            outputStream.write(out_data);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static String encodeMessage(String string) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < string.length(); i++) {
-            sb.append(string.charAt(i));
-            sb.append(string.charAt(i));
-            sb.append(string.charAt(i));
+    public static void send() {
+        Random r = new Random();
+        File file = new File("encoded.txt");
+        int numBytes = (int) file.length();
+        byte[] data = new byte[numBytes];
+
+        try (FileInputStream inputStream = new FileInputStream("encoded.txt")) {
+            inputStream.read(data);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return sb.toString();
+
+        for (int i = 0; i < numBytes; ++i) {
+            int pos = r.nextInt(8);
+            data[i] ^= (1 << pos);
+        }
+
+        for (var i : data) {
+            System.out.print(i + " ");
+        }
+
+        try (OutputStream outputStream = new FileOutputStream("received.txt", false)) {
+            outputStream.write(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static String decodeMessage(String string) {
-        StringBuilder sb = new StringBuilder();
-        char[] input = string.toCharArray();
+    public static byte decodeBytes(byte input) {
+        int xor = 0;
+        for (int i = 0; i < 3; i++) {
+            if (getBit(input, i * 2) == getBit(input, i * 2 + 1))
+                xor ^= getBit(input, i * 2);
+        }
 
-        for (int i = 1; i < input.length - 1; i +=3) {
-            if (input[i] == input[i - 1] || input[i - 1] == input[i + 1]) {
-                sb.append(input[i - 1]);
-            } else {
-                sb.append(input[i]);
+        for (int j = 0; j < 3; ++j) {
+            if (getBit(input, j * 2) != getBit(input, j * 2 + 1)) {
+                input = setBit(input, xor ^ getBit(input, 6), j * 2);
+                input = setBit(input, xor ^ getBit(input, 6), j * 2 + 1);
+                return input;
             }
         }
-        return sb.toString();
+
+        input = setBit(input, xor , 6);
+        input = setBit(input, xor , 7);
+
+        return input;
     }
+
+    public static void decode() {
+        File file = new File("received.txt");
+        int numBytes = (int) file.length();
+        int numOutBytes = (numBytes * 3) / 8;
+        byte[] data = new byte[numBytes];
+        byte[] out_data = new byte[numOutBytes];
+
+        try (FileInputStream inputStream = new FileInputStream("received.txt")) {
+            inputStream.read(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < data.length; ++i) {
+            data[i] = decodeBytes(data[i]);
+            for (int j = 0; j < 3; j++) {
+                int bitIndex = i * 3 + j;
+                int byteIndex = bitIndex / 8;
+                int bytePos = bitIndex % 8;
+                int bit = getBit(data[i], j * 2);
+                if (byteIndex < numOutBytes) {
+                    out_data[byteIndex] = setBit(out_data[byteIndex], bit, bytePos);
+                }
+            }
+        }
+
+        for (var i : out_data) {
+            System.out.print(i + " ");
+        }
+
+        try (OutputStream outputStream = new FileOutputStream("decoded.txt", false)) {
+            outputStream.write(out_data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
